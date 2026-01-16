d5a7ac866b635a731d8c470da995d28677fda3ea
Cynthia Agata Limantono <cea@student.ubc.ca>
2026-01-15 22:08:52 -0800

    wrote function code draft #26

--- src/dsci_524_group_30_data_validation/missing_values_validate.py
+++ src/dsci_524_group_30_data_validation/missing_values_validate.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def missing_values_validate(df: pd.DataFrame, col: str | int, threshold: float | int) -> bool:
     """
     Validate the amount of missing values in a pandas DataFrame.
@@ -47,3 +49,30 @@ def missing_values_validate(df: pd.DataFrame, col: str | int, threshold: float |
     >>> missing_values_validate(df=data, col="name", threshold=0.05)
     False
     """
+
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("Input 1 must be a pandas Dataframe")
+
+    if not isinstance(col, (str, int)):
+        raise TypeError("Input 2 must be a string or integer")
+    
+    if not isinstance(threshold, (float, int)):
+        raise TypeError("Input 3 must be numeric")
+    
+    if threshold < 0:
+        raise ValueError("Threshold is invalid: larger than 1")
+    
+    if threshold > 1:
+        raise ValueError("Threshold is invalid: negative threshold")
+    
+    if col is not in df.columns:
+        raise KeyError("Column is not found in the Dataframe")
+
+    na_frac = df[col].isna().mean()
+
+    if na_frac <= threshold:
+        print("The amount of missing values are valid. Checks completed!")
+        return True
+    else:
+        print(f"Invalid check: the amount of missing values is {na_frac}, exceeding the threshold: {threshold}. Checks completed!")
+        return False
