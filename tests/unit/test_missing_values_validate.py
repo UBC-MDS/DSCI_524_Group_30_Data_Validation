30d8e46d6fb1a93cf063c8d0449466a973b062da
Cynthia Agata Limantono <cea@student.ubc.ca>
2026-01-15 21:28:57 -0800

    Implemented the tests and improved it with Claude's suggestion #25

--- tests/unit/test_missing_values_validate.py
+++ tests/unit/test_missing_values_validate.py
@@ -13,32 +13,65 @@ def sample_dataframe():
         "name": ["Alex", None, None, "Austin", None],
         "age": [21, 43, 23, None, 38],
         "sex": ["M", "F", "F", "M", "F"],
-        "married": [True, False, None, None, True]
+        "married": [True, False, None, None, True],
+        "job": [None, None, None, None, None]
     })
 
-def test_missing_values_validate():
+@pytest.mark.parametrize(
+    "col, threshold, expected_out",
+    [
+        ("age", 0.25, True), #below threshold
+        ("sex", 0.0, True), #exactly at the threshold, col with no missing values
+        (0, 0.25, False), # above threshold, int col 
+        ("age", 0.20, True), # exactly at the threshold
+        ("married", 0.39, False), # just right above the threshold
+        (4, 0.99, False), # edge below threshold, int col
+        ("job", 1.0, True) # edge at exact threshold
+    ]
+)
+
+@pytest.mark.parametrize(
+    "df, col, threshold, error_str",
+    [
+        ({"name": ["Alex"]}, "name", 0.05, "Input 1 must be a pandas Dataframe"),
+        (sample_dataframe, ["1", "name"], 0.1, "Input 2 must be a string or integer"),
+        (sample_dataframe, "age", "0.20", "Input 3 must be numeric"),
+        (["Alex", None], ["name"], {"1": 0.05}, "Input 1 must be a pandas Dataframe."),
+    ]
+)
+
+def test_missing_values_validate(sample_dataframe, col, threshold, expected_out):
     """
     Test that missing_values_validate works as expected for example below,
      above, and at exact the threshold.
     """
+    out = missing_values_validate(sample_dataframe, col, threshold)
+    assert  expected_out == out, f"Expected {expected_out} but got {out}"
 
-def test_missing_values_validate_invalid_col():
+def test_missing_values_validate_invalid_col(sample_dataframe):
     """
     Test that missing_values_validate raises exception for column not found in dataframe.
     """
+    with pytest.raises(KeyError, match="Column is not found in the dataframe."):
+        missing_values_validate(sample_dataframe, "1", 0.23)
+    with pytest.raises(KeyError, match="Column is not found in the dataframe."):
+        missing_values_validate(sample_dataframe, "", 0.23)
 
-def test_missing_values_validate_invalid_threshold():
+def test_missing_values_validate_invalid_threshold(sample_dataframe):
     """
     Test that missing_values_validate raises exception for invalid threshold.
     Threshold must be >= 0 and <= 1
     """
+    with pytest.raises(ValueError, match="Threshold is invalid: negative threshold"):
+        missing_values_validate(sample_dataframe, "name", -0.01)
+    with pytest.raises(ValueError, match="Threshold is invalid: larger than 1"):
+        missing_values_validate(sample_dataframe, "age", 1.01)
 
-def test_missing_values_validate_invalid_input_type():
+def test_missing_values_validate_invalid_input_type(df, col, threshold, error_str):
     """
     Test that missing_values_validate raises exception for inputs with incorrect data types.
     """
-
-
-
-
-
+    with pytest.raises(TypeError, match=error_str):
+        missing_values_validate(df, col, threshold)
+    with pytest.raises(AttributeError, match="Dataframe cannot be None."):
+        missing_values_validate(None, "age", 0.2)
